56f4772dbd8879bd8b3a46ec536c878412593927
Galip Yıldız <70694743+galipyildiz@users.noreply.github.com>
2023-12-27 11:37:30 +0300

    Update config.js

--- demoproject.ui/src/env/config.js
+++ demoproject.ui/src/env/config.js
@@ -1,3 +1,3 @@
-export const apiUrl = "https://glpdemoproject.herokuapp.com/api/"
+export const apiUrl = "https://glpdemoproject-056f5c4b4508.herokuapp.com/api/"
 // export const apiUrl = "https://localhost:44350/api/"
 export default apiUrl;
